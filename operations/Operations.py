bc76a697b419ad56d07f7d87a90876913a4cfb2e
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-03-28 14:44:53 -0300

    Alterações da saída das casas decimais (limitado a 2 casas)

--- operations/Operations.py
+++ operations/Operations.py
@@ -3,14 +3,14 @@ num = int( input("Insira o número a ser calculado: "))
 
 print(f"\nSoma\n------------------\n")
 for i in range(11):
-    print(f"{num} + {i} = ",num+i )
+    print(f"{num} + {i} = {num+i}")
 print(f"\nSubtração\n------------------\n")
 for i in range(11):
-    print(f"{num} - {i} = ",num-i )
+    print(f"{num} - {i} = {num-i}")
 print(f"\nMultiplicação\n------------------\n")
 for i in range(11):
-    print(f"{num} * {i} = ", num * i)
+    print(f"{num} * {i} = {num * i}")
 print(f"\nDivisão\n------------------\n")
 for i in range(11):
     if(i>0):
-        print(f"{num} / {i} = ",num/i )
+        print(f"{num} / {i} = {num/i:.2f} " )
